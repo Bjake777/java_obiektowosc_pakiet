e30494d02ef217036ea1d901eddad193d2f5c8b6
Michal Bujak <michalbujak7777@gmail.com>
2021-08-12 16:45:03 +0200

    dodanie zadania nr 2

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import permute.Permute;
+
+import java.util.Arrays;
+import permute.Permute;
+public class Main {
+    public static void main(String[] args) {
+
+        Permute.permute(Arrays.asList("ala", "ma", "kota"), 0);
+    }
+}
